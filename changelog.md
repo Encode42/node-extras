632276c3c096f3bba7da5a7895a9de59841da186
Encode42 <me@encode42.dev>
2022-11-28 15:59:05 -0500

    1.0.1

--- changelog.md
+++ changelog.md
@@ -1,2 +1,6 @@
+# 1.0.1
+### Added
+- The `ClassSpread` type.
+
 # 1.0.0
 Initial release
--- src/type/Class.ts
+++ src/type/Class.ts
@@ -2,3 +2,8 @@
  * Generic class type.
  */
 export type Class<Clazz, Args> = new (args: Args) => Clazz;
+
+/**
+ * Generic class type with spread operator for arguments.
+ */
+export type ClassSpread<Clazz, SpreadType> = new (...args: SpreadType[]) => Clazz;
